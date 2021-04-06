5ea5299f00aee2972478476a4f5f384321b0bcfd
Haoming <hchen549@wisc.edu>
2021-04-05 22:04:12 -0500

    Activity 10

--- js/lab2.js
+++ js/lab2.js
@@ -10,6 +10,25 @@ var measures = [
 ];
 var expressed = "vaccinated";
 
+var chartWidth = window.innerWidth * 0.55,
+  chartHeight = 460,
+  leftPadding = 25,
+  rightPadding = 2,
+  topBottomPadding = 5,
+  chartInnerWidth = chartWidth - leftPadding - rightPadding,
+  chartInnerHeight = chartHeight - topBottomPadding * 2,
+  translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+// var yScale = d3
+//   .scaleLinear()
+//   .range([chartHeight, 0])
+//   .domain([
+//     0,
+//     d3.max(attributes, (d) => {
+//       return parseFloat(d[expressed]);
+//     }),
+//   ]);
+
 function setMap() {
   let tooltip = d3.select("#tooltip");
   var width = window.innerWidth * 0.4;
@@ -35,17 +54,9 @@ function setMap() {
   Promise.all(promises).then(callback);
 
   function callback(data) {
-    // var map = d3
-    //   .select("body")
-    //   .append("svg")
-    //   .attr("class", "map")
-    //   .attr("width", width)
-    //   .attr("height", height);
-    // var g = map.append("g");
     attributes = data[0];
     usmap = data[1];
     console.log(attributes);
-    // console.log(usmap);
 
     var states = topojson.feature(usmap, usmap.objects.states);
     // console.log(states);
@@ -53,76 +64,15 @@ function setMap() {
     joinData(states, attributes);
 
     console.log(states);
-    // california = states.features.filter(
-    //   (f) => f.properties.name === "California"
-    // );
-    // projection = d3.geoAlbersUsa().fitSize([960, 660], california[0]); // magically fits map geography to width/height!
+
     projection = d3.geoAlbersUsa().fitSize([width, height], states);
     path = d3.geoPath(projection);
-    // console.log("State features");
-    // console.log(states.features);
-    // console.log(states.features[0]["id"]);
 
     var colorScale = makeColorScale(attributes);
 
-    // var regions = setEnumerationUnits(states, g, path, colorScale);
-    var regions = map
-      .selectAll("path")
-      .data(states.features)
-      .enter()
-      .append("path")
-      .attr("class", "usstate")
-      .attr("id", function (state) {
-        // console.log(state.id);
-        return state.properties.name;
-      })
-      .attr("d", (state) => path(state))
-      //   .attr()
-      .attr("fill", "white")
-      .style("fill", (d) => {
-        console.log(d["properties"][expressed]);
-        return colorScale(d["properties"][expressed]);
-      })
-      .append("title")
-      .text((state) => `${state.properties.name}`);
-
-    // map
-    //   .append("g")
-    //   .attr("transform", "translate(610,20)")
-    //   .append(() =>
-    //     legend({ colorScale, title: "D3 lab", width: 260, tickFormat: ".1f" })
-    //   );
+    var regions = setEnumerationUnits(states, map, path, colorScale);
 
     setChart(attributes, colorScale);
-
-    // console.log("region: " + regions);
-    // var legends = regions
-    //   .selectAll("title")
-    //   .data(states.features)
-    //   .enter()
-    //   .append("title")
-    //   .text((state) => `${state.properties.name}`);
-    //   .on("mouseover", function (state) {
-    //     tooltip.transition().style("visibility", "visible");
-    //     console.log(map.select(".Wisconsin"));
-    //     // tooltip.text("abchdkkl: " + id);
-    //     // tooltip.attr("", st);
-    //   })
-    //   .on("mouseout", (countyDataItem) => {
-    //     tooltip.transition().style("visibility", "hidden");
-    //   });
-
-    // var zoom = d3
-    //   .zoom()
-    //   .scaleExtent([1, 8])
-    //   .on("zoom", function () {
-    //     d3.select("g").attr(
-    //       "transform",
-    //       "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
-    //     );
-    //   });
-
-    // map.call(zoom);
   }
 }
 
@@ -146,25 +96,28 @@ function joinData(states, attributes) {
 }
 
 function makeColorScale(data) {
+  var min = d3.min(data, (d) => {
+    return parseFloat(d[expressed]);
+  });
+
+  var max = d3.max(data, (d) => {
+    return parseFloat(d[expressed]);
+  });
+
+  console.log(min, max);
+
   var colorScale = d3
     .scaleLinear()
     .range(["yellow", "red"])
-    .domain([
-      d3.min(data, (d) => {
-        return parseFloat(d[expressed]);
-      }),
-      d3.max(data, (d) => {
-        return parseFloat(d[expressed]);
-      }),
-    ])
+    .domain([min, max])
     .unknown("#ccc");
 
   return colorScale;
 }
 
 function setEnumerationUnits(states, map, path, colorScale) {
-  var regions = g
-    .selectAll("path")
+  var regions = map
+    .selectAll(".regions")
     .data(states.features)
     .enter()
     .append("path")
@@ -180,38 +133,20 @@ function setEnumerationUnits(states, map, path, colorScale) {
       console.log(d["properties"][expressed]);
       return colorScale(d["properties"][expressed]);
     })
+    .on("mouseover", function (event, state) {
+      highlightState(state.properties.name);
+    })
     .append("title")
     .text((state) => `${state.properties.name}`);
 
-  // console.log("region: " + regions);
-  // var legends = regions
-  //   .selectAll("title")
-  //   .data(states.features)
-  //   .enter()
-  //   .append("title")
-  //   .text((state) => `${state.properties.name}`);
-  //   .on("mouseover", function (state) {
-  //     tooltip.transition().style("visibility", "visible");
-  //     console.log(map.select(".Wisconsin"));
-  //     // tooltip.text("abchdkkl: " + id);
-  //     // tooltip.attr("", st);
-  //   })
-  //   .on("mouseout", (countyDataItem) => {
-  //     tooltip.transition().style("visibility", "hidden");
-  //   });
+  var desc = regions
+    .append("desc")
+    .text('{"stroke": "#000", "stroke-width": "0.5px"}');
+
   return regions;
 }
 
 function setChart(attributes, colorScale) {
-  var chartWidth = window.innerWidth * 0.55,
-    chartHeight = 460,
-    leftPadding = 25,
-    rightPadding = 2,
-    topBottomPadding = 5,
-    chartInnerWidth = chartWidth - leftPadding - rightPadding,
-    chartInnerHeight = chartHeight - topBottomPadding * 2,
-    translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
-
   var yScale = d3
     .scaleLinear()
     .range([chartHeight, 0])
@@ -245,44 +180,31 @@ function setChart(attributes, colorScale) {
       return b[expressed] - a[expressed];
     })
     .attr("class", (d) => {
-      return "bars_" + d["states"];
+      return "bars";
+    })
+    .attr("id", (d) => {
+      return d.states;
     })
     .attr("width", chartWidth / attributes.length - 1)
-    .attr("x", function (d, i) {
-      return i * (chartWidth / attributes.length) + leftPadding;
-    })
-    .attr("height", (d) => {
-      return chartHeight - yScale(parseFloat(d[expressed]));
-    })
-    .attr("y", (d) => {
-      return yScale(parseFloat(d[expressed])) + topBottomPadding;
-    })
-    .style("fill", function (d) {
-      return colorScale(d[expressed]);
+    .on("mouseover", (event, d) => {
+      console.log(d);
+      highlightState(d.states);
     });
 
-  // var numbers = chart
-  //   .selectAll(".numbers")
-  //   .data(attributes)
-  //   .enter()
-  //   .append("text")
-  //   .sort((a, b) => {
-  //     return b[expressed] - a[expressed];
-  //   })
-  //   .attr("class", (d) => {
-  //     return "numbers_" + d["states"];
-  //   })
-  //   .attr("text-anchor", "middle")
-  //   .attr("x", (d, i) => {
-  //     var fraction = chartWidth / attributes.length;
-  //     return i * (chartWidth / attributes.length) + (fraction - 1) / 2;
-  //   })
-  //   .attr("y", (d) => {
-  //     return chartHeight - yScale(parseFloat(d[expressed])) + 15;
-  //   })
-  //   .text((d) => {
-  //     return d[expressed];
-  //   });
+  // .attr("x", function (d, i) {
+  //   return i * (chartWidth / attributes.length) + leftPadding;
+  // })
+  // .attr("height", (d) => {
+  //   return chartHeight - yScale(parseFloat(d[expressed]));
+  // })
+  // .attr("y", (d) => {
+  //   return yScale(parseFloat(d[expressed])) + topBottomPadding;
+  // })
+  // .style("fill", function (d) {
+  //   return colorScale(d[expressed]);
+  // });
+
+  updateChart(bars, attributes.length, colorScale);
 
   //create vertical axis generator
   var yAxis = d3.axisLeft().scale(yScale);
@@ -293,4 +215,131 @@ function setChart(attributes, colorScale) {
     .attr("class", "axis")
     .attr("transform", translate)
     .call(yAxis);
+
+  createDropdown(attributes);
+}
+
+function updateChart(bars, n, colorScale) {
+  // Update yScale depending on selected variable
+  var yScale = d3
+    .scaleLinear()
+    .range([chartHeight, 0])
+    .domain([
+      0,
+      d3.max(attributes, (d) => {
+        return parseFloat(d[expressed]);
+      }),
+    ]);
+
+  // Update chart title
+  d3.select(".chart")
+    .select("text")
+    .text("Number of Variable " + expressed + " in each state");
+
+  // Update bar itself
+  bars
+    .attr("x", function (d, i) {
+      return i * (chartInnerWidth / n) + leftPadding;
+    })
+    //size/resize bars
+    .attr("height", function (d, i) {
+      return 463 - yScale(parseFloat(d[expressed]));
+    })
+    .attr("y", function (d, i) {
+      return yScale(parseFloat(d[expressed])) + topBottomPadding;
+    })
+    //color/recolor bars
+    .style("fill", function (d) {
+      var value = d[expressed];
+      if (value) {
+        return colorScale(value);
+      } else {
+        return "#ccc";
+      }
+    });
+}
+
+function createDropdown(attributes) {
+  var dropdown = d3
+    .select("body")
+    .append("select")
+    .attr("class", "dropdown")
+    .on("change", function () {
+      console.log("change attribues");
+      return changeAttributes(this.value, attributes);
+    });
+
+  var titleOption = dropdown
+    .append("option")
+    .attr("class", "titleOption")
+    .attr("disabled", "true")
+    .text("Select Attribute");
+
+  var attrOptions = dropdown
+    .selectAll("attrOptions")
+    .data(measures)
+    .enter()
+    .append("option")
+    .attr("value", (d) => {
+      return d;
+    })
+    .text((d) => {
+      return d;
+    });
+}
+
+function changeAttributes(option, attributes) {
+  expressed = option;
+
+  var colorScale = makeColorScale(attributes);
+
+  console.log(expressed);
+
+  var yScale = d3
+    .scaleLinear()
+    .range([chartHeight, 0])
+    .domain([
+      0,
+      d3.max(attributes, (d) => {
+        return parseFloat(d[expressed]);
+      }),
+    ]);
+
+  var regions = d3
+    .selectAll(".usstate")
+    .transition()
+    .duration(1000)
+    .style("fill", function (d) {
+      // console.log(expressed);
+      var value = d.properties[expressed];
+      if (value) {
+        // console.log(value);
+        return colorScale(value);
+      } else {
+        return "#ccc";
+      }
+    });
+
+  console.log("change bars");
+
+  var bars = d3
+    .selectAll(".bars")
+    //re-sort bars
+    .sort(function (a, b) {
+      console.log("a");
+      return b[expressed] - a[expressed];
+    })
+    .transition()
+    .delay((d, i) => {
+      return i * 20;
+    })
+    .duration(500);
+
+  updateChart(bars, attributes.length, colorScale);
+}
+
+function highlightState(state) {
+  d3.selectAll("#" + state)
+    .style("stroke", "blue")
+    .style("stroke-width", "2");
 }
